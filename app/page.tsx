cabef04d778c4099aec81161df6d83ed5f5013e4
Danish <danish.anzari@ariantechsolutions.com>
2025-12-31 21:05:54 +0530

    remove unnecessary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,16 @@
 import { Banner } from "@/components/banner";
 import Footer from "@/components/footer";
-import { Hero } from "@/components/hero";
-import { Nav } from "@/components/nav";
 import { NewHero } from "@/components/new-hero";
 import { NewNav } from "@/components/new-nav";
 import { OurService } from "@/components/our-service";
 import { Pricing } from "@/components/pricing";
-import { Work } from "@/components/work";
 
 export default function Page() {
   return (
     <>
-      {/* <Nav /> */}
       <NewNav />
       <Banner />
-      {/* <Hero /> */}
       <NewHero />
-      <Work />
-      <OurService />
-      <Pricing />
-      <Footer />
     </>
   );
 }
