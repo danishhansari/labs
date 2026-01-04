3f01e1e8c3f8e2cc961594ebf74788462c5dce12
Danish <danish.anzari@ariantechsolutions.com>
2026-01-04 17:30:05 +0530

    working on responsizeness

--- components/problem.tsx
+++ components/problem.tsx
@@ -10,8 +10,8 @@ import { ForegroundButton } from "./ui/foreground-button";
 
 export const Problem = () => {
   return (
-    <div className="bg-white py-14 text-[#040E0E]">
-      <div className="relative px-4 sm:px-8 lg:px-18 border-y border-zinc-200">
+    <div className="bg-white py-12 md:py-14 text-[#040E0E]">
+      <div className="relative px-5 sm:px-8 lg:px-18 border-y border-zinc-200">
         <div className="absolute left-0 -top-1 w-full flex justify-between px-3 sm:px-6 lg:px-16">
           <span className="w-2.5 h-2.5 rounded-full bg-white border border-zinc-200 ml-1 m z-10"></span>
           <span className="w-2.5 h-2.5 rounded-full bg-white border border-zinc-200 mr-1 z-10"></span>
@@ -23,16 +23,16 @@ export const Problem = () => {
         <div className="border-x border-zinc-200 flex flex-col lg:flex-row gap-6 items-stretch">
           <div className="pt-5 w-full">
             <h2
-              className={`px-6 text-[#FF5858] uppercase ${ibmPlexMono.className} text-base`}
+              className={`px-6 text-[#FF5858] uppercase ${ibmPlexMono.className} text-sm md:text-base`}
             >
               problems
             </h2>
             <p
-              className={`px-4 pl-6 text-5xl ${romanFont.className} mt-3 tracking-tight`}
+              className={`px-4 pl-6 text-3xl font-normal md:text-5xl ${romanFont.className} mt-3 tracking-tight`}
             >
               Product building shouldn&apos;t be this complicated
             </p>
-            <ForegroundButton className="mx-7 mt-8" />
+            <ForegroundButton className="mx-7 mt-6 md:mt-8" />
 
             <ul className="mt-12">
               <li className="flex items-center gap-3 border-b border-zinc-200 py-4 px-8">
@@ -57,7 +57,7 @@ export const Problem = () => {
                     className="block"
                   />
                 </span>
-                <span className="text-lg">
+                <span className="text-sm md:text-lg">
                   Slow timelines and unclear progress
                 </span>
               </li>
